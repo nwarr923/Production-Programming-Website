b01aba6edccc8d5125875cdb0c8d11a9e8f063af
Kelly Buege <32723772+kbuege@users.noreply.github.com>
2018-10-22 10:33:37 -0500

    Final Results Page

--- results.php
+++ results.php
@@ -27,30 +27,11 @@ if (!$db->getConnStatus()) {
   exit;
 }
 
-/*
-//INSERT example
-//Pretend this is unsanitized
-//user data from a form:
-$user = "bob";
-$safeUser = $db->dbEsc($user);
-
-$query = "INSERT INTO testschema (username,pass,active) " .
-          "VALUES ('{$safeUser}','l33t',1)";
-$result = $db->dbCall($query);
-
-//This will contain the insert id
-print "Insert statement executed, insert id was: " . $result . "\n";
-*/
-
-//If using unsanitized data, be sure
-//to call the dbEsc() method on any individual values!
-// Must do that prior to building the statement here
-
-$query = "SELECT * FROM bookinfo";
+$query = "Select * FROM bookinfo HAVING bookinfo.booktitle LIKE '$bookInfo'
+OR bookinfo.isbn LIKE '$bookInfo' OR bookinfo.author LIKE '$bookInfo'";
 
 $result = $db->dbCall($query);
 
-$safeResult = $db->dbEsc($result);
 
 $page = new Template("results.php");
 $page->setHeadSection("<link rel='stylesheet' type='text/css' href='headerStyles.css'/>");
@@ -62,7 +43,7 @@ print $page->getTopSection();
 print "<div class='size-wrapper'>\n";
 print "<header>\n";
 print "<a id='login' href='#'>Login</a>\n";
-print "<a id='siteTitle' href='home.php'>CNMT 310 Group 1!</a>\n";
+print "<a id='siteTitle' href='home.php'>Assignment 1 Website</a>\n";
 print "<nav>\n";
 print "<ul class='navbar'>\n";
 print "<li><a href='home.php'>Home</a></li>\n";
@@ -75,44 +56,83 @@ print "</header>\n";
 
 print "<div class='content'>\n";
 print "<div id='content'>\n";
-print "<h1>Here are your results</h1>\n";
+print "<h1>Here are your results: </h1>\n";
 print "<table>";
-foreach ($result as $row) {
-	print "<tr>";
-	
-	print "<td>";
-	print ($row['id']); 
-	print "</td>";
-	
-	print "<td>";
-	print ($row['inserttime']); 
-	print "</td>";
 
+if(!empty($result) ) {
+
+	foreach ($result as $row) {
 	
-	print "<td>";
-	print ($row['booktitle']); 
-	print "</td>";
+		print "<tr>";
+			print "<td>";
+			print "<p> ID Number: </p>";
+			print "</td>";
+			
+			print "<td class='spacer'>";
+			print ($row['id']); 
+			print "</td>";
+		print "</tr>";
+		
+		print "<tr>";
+			print "<td>";
+			print "<p> Insert Time: </p>";
+			print "</td>";
+			
+			print "<td class='spacer'>";
+			print ($row['inserttime']); 
+			print "</td>";
+		print "</tr>";
+
+		print "<tr>";
+			print "<td>";
+			print "<p> Book Title: </p>";
+			print "</td>";
+			
+			print "<td class='spacer'>";
+			print ($row['booktitle']); 
+			print "</td>";
+		print "</tr>";
+		
+		print "<tr>";
+			print "<td>";
+			print "<p> ISBN: </p>";
+			print "</td>";
+			
+			print "<td class='spacer'>";
+			print ($row['isbn']); 
+			print "</td>";
+		print "</tr>";
+		
+		print "<tr>";
+			print "<td>";
+			print "<p> Author: </p>";
+			print "</td>";
+		
+			print "<td class='spacer'>";
+			print ($row['author']); 
+			print "</td>";
+		print "</tr>";
 	
-	print "<td>";
-	print ($row['isbn']); 
-	print "</td>";
+		print "<tr>";
+			print "<td>";
+			print "<p> Status: </p>";
+			print "</td>";
+		
+			print "<td class='spacer'>";
+			print ($row['status']); 
+			print "</td>";
+		print "</tr>";
 	
-	print "<td>";
-	print ($row['author']); 
-	print "</td>";
-	
-	print "<td>";
-	print ($row['status']); 
-	print "</td>";
-	
-	print "</tr>";
+	}
+} else {
+	print "<h1> Item Not Found </h1>";
 }
 print "</table>";
 print "</div>\n";
 print "</div>\n";
 
 print "<footer>\n";
-print "<p>CNMT 310, Fall Semester, Group 1</p>\n";
+print "<p>Assignment 1 Footer. Noah Warren, CNMT 310, 9/30/2018</p>\n";
 print "</footer>\n";
 print "</div>\n";
 print $page->getBottomSection();
